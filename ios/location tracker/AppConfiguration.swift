d1adcff0b60717ef76abdef4aeffafb37f29e93f
Alpakk4 <alexskondras@icloud.com>
2026-02-03 20:03:09 +0000

    Modify ConfigurationKeys for better clarity
    Updated apikey and uid values for clarity.

--- ios/location tracker/AppConfiguration.swift
+++ ios/location tracker/AppConfiguration.swift
@@ -3,11 +3,12 @@
 //  location tracker
 //
 //  Created by Joel on 01/09/2025.
-//
+
+// original values "apikey","endpoint","uid"
 
 struct ConfigurationKeys {
-    static let apikey = "apikey"
+    static let apikey = "apikey" // justtext to display in app
     static let endpoint = "endpoint"
-    static let uid = "uid"
+    static let uid = "unspecified_ios_device"
     static let enableReporting = "enableReporting"
 }
